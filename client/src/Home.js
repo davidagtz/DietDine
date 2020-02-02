b222b06bccd87ed3935eda581b64868c073cc0a5
SanjithChockan <sanjith.miapsung@gmail.com>
2020-02-01 19:12:49 -0600

    Updated search bar

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,24 +1,54 @@
 import React, { Children } from "react";
+import ReactSearchBox from "react-search-box";
 import "./styles/App.sass";
 
 function App() {
-	let children = [];
-	for(const city of ["Richardson", "Plano", "Frisco", "McKinney", "Victory Park"])
-		children.push(<a href="/map">{city}</a>)
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1 className="App-Title">
-					DIETDINE
-				</h1>
-				<input type="text" className="input-Searchbar" placeholder="Enter location..." />
-				<ul className = "closest">
-					{children}
-				</ul>
-			</header>
-		</div>
-
-	);
+  let data = [
+    {
+      key: "plano",
+      value: "Plano"
+    },
+    {
+      key: "richardson",
+      value: "Richardson"
+    },
+    {
+      key: "mckinney",
+      value: "McKinney"
+    },
+    {
+      key: "frisco",
+      value: "Frisco"
+    },
+    {
+      key: "victory",
+      value: "Victory Park"
+    }
+  ];
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-Title">DIETDINE</h1>
+        <ReactSearchBox
+          placeholder="Enter a place"
+          data={data}
+          onSelect={record => {
+            fetch("/geocode/" + record).then(res => {
+              window.location.href = "./map?location=" + res;
+            });
+          }}
+          onFocus={() => {
+            console.log("This function is called when is focussed");
+          }}
+          onChange={value => console.log(value)}
+          fuseConfigs={{
+            threshold: 0.05
+          }}
+          inputFontColor="#f00"
+        />
+      </header>
+    </div>
+  );
 }
 
 export default App;
