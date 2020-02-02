ae4293f3809336f0a8f51f23cb63b9ecd2df4124
armaanmitha <armaan.mitha@gmail.com>
2020-02-02 06:24:47 -0600

    Adds sidebar restaurant tiles

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -5,11 +5,15 @@ class PopUp extends Component {
 	render () {
 		return (
 			<Modal trigger={this.props.trigger ? this.props.trigger : <Button>{this.props.title}</Button>}>
-				<Modal.Header>Select a Photo</Modal.Header>
+				<Modal.Header>Taco Bell</Modal.Header>
 				<Modal.Content image>
-					<Image wrapped size="medium" src="{this.props.image}" />
+					<Image wrapped size="medium" src="/images/tacobell.jpg" />
 					<Modal.Description>
-						<Header>Default Profile Image</Header>
+						<Header>Menu Items</Header>
+						<ul>
+							<li>Ey taco taco taco taco</li>
+							<li>Freeze! everybody clap yo hands</li>
+						</ul>
 						{this.props.children}
 					</Modal.Description>
 				</Modal.Content>
